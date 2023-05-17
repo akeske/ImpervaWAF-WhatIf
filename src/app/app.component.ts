6d4610cdfc42fe71bfb5b0f11312a0563454e679
Thanos Keskempes <akeske@users.noreply.github.com>
2023-05-17 18:58:43 +0300

    more comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { parseScript } from 'esprima';
 import {
   SecurityRule,
   IPReputationRiskLevelEnum,
@@ -8,8 +10,6 @@ import {
   RuleSet,
   RuleExpression,
 } from './model';
-import { parseScript } from 'esprima';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +18,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent {
   ruleStrings: string[] = [
+    '(ASN == 123 & ASN != 123) | (ASN == 123 & (ASN != 123 | (ASN == 123 & ASN != 123)))',
+    '(ASN == 123 & ASN != 123) | ((ASN == 123 & ASN != 123) | (ASN == 123 & ASN != 123))',
     'ClientType == HackingTool & (ASN != 29241 & ASN != 8075 & ASN != 56910)',
     'ClientType == HackingTool & (ASN != 29241;8075;56910)',
     'MaliciousIPList == TorIPs;AnonymousProxyIPs',
@@ -27,12 +29,18 @@ export class AppComponent {
     'ClientType == DDoSBot;Worm;MaskingProxy;ClickBot;CommentSpamBot;SpamBot;VulnerabilityScanner & ClientId != 453',
     '(URL not-contains "oauth";"saml") & (URL not-contains "/dashboard" | ( ClientIP != 62.103.236.223 & ClientIP != 20.50.146.69 & ClientIP != 20.73.36.250 & ClientIP != 62.169.201.60 & ClientIP != 62.103.236.223 & ClientIP != 62.169.197.77))',
   ];
+
   fakeUser: SecurityRule = {};
-  ruless: RuleSet[] = [];
+  rules: RuleSet[] = [];
 
   fakeUserForm = new FormGroup({
-    ClientType: new FormControl(localStorage.getItem('ClientType'), Validators.required),
-    IPReputationRiskLevel: new FormControl(localStorage.getItem('IPReputationRiskLevel')),
+    ClientType: new FormControl(
+      localStorage.getItem('ClientType'),
+      Validators.required
+    ),
+    IPReputationRiskLevel: new FormControl(
+      localStorage.getItem('IPReputationRiskLevel')
+    ),
     ASN: new FormControl(localStorage.getItem('ASN')),
     CountryCode: new FormControl(localStorage.getItem('CountryCode')),
     ClientIP: new FormControl(localStorage.getItem('ClientIP'), [
@@ -64,63 +72,46 @@ export class AppComponent {
       MaliciousIPList: form.value.MaliciousIPList,
     };
     localStorage.setItem('ClientType', form.value.ClientType);
-    localStorage.setItem('IPReputationRiskLevel', form.value.IPReputationRiskLevel);
+    localStorage.setItem(
+      'IPReputationRiskLevel',
+      form.value.IPReputationRiskLevel
+    );
     localStorage.setItem('ASN', form.value.ASN);
     localStorage.setItem('CountryCode', form.value.CountryCode);
     localStorage.setItem('ClientIP', form.value.ClientIP);
     localStorage.setItem('ClientId', form.value.ClientId);
     localStorage.setItem('URL', form.value.URL);
     localStorage.setItem('MaliciousIPList', form.value.MaliciousIPList);
-    this.ruless = [];
+
+    this.rules = [];
+
     this.parseRules();
   }
 
-  protected evaluate = (node: any): boolean => {
-    if (node.type === 'LogicalExpression' || node.type === 'BinaryExpression') {
-      const left = this.evaluate(node.left);
-      const right = this.evaluate(node.right);
-      switch (node.operator) {
-        case '&':
-          return left && right;
-        case '|':
-          return left || right;
-        default:
-          throw new Error(`Unknown operator: ${node.operator}`);
-      }
-    } else if (node.type === 'UnaryExpression') {
-      return !node.argument;
-    } else if (node.type === 'ExpressionStatement') {
-      return this.evaluate(node.expression);
-    } else if (node.type === 'Literal') {
-      return node.value;
-    } else if (node.type === 'Identifier') {
-      // Assume that the identifier refers to a boolean value in the environment.
-      return node.value;
-    } else {
-      throw new Error(`Unknown node type: ${node.type}`);
-    }
-  };
-
   protected parseRules(): void {
     const separators = ['&', '\\|', '\\(', '\\)'];
-    let res = '';
+    // the result of a rule, for example
+    // ClientID == 14 & ClientIP == 55.66.44.22
+    // if user's ClientID = 14 and ClientIP = 11.22.33.44
+    // then the vvariable ruleBooleanFormat will be 'T & F'
+    let ruleBooleanFormat = '';
     // get the rules
     this.ruleStrings.forEach(ruleString => {
       // create a new RuleSet object to store the rule string, results and expressions
       let ruleSet: RuleSet = new RuleSet();
 
-      // set the rule from ruleString 
+      // set the rule from ruleString
       ruleSet.rule = ruleString.replace(/;/g, ' ; ');
-      
+
       // split the rulestring depends on '&,|,(,)' chars
-      let ruleExpression = ruleString.split(
+      let ruleExpressions = ruleString.split(
         new RegExp(separators.join('|'), 'g')
       );
 
       // get one-by-one the rule expressions 'type == value'
       let operator = '';
       let parenthesis = '';
-      ruleExpression.forEach(rule => {
+      ruleExpressions.forEach(rule => {
         rule = rule.trim();
         let type = '';
         let vars: string[] = [];
@@ -162,8 +153,6 @@ export class AppComponent {
           if (type === key) {
             vars.forEach(vari => {
               if (key === type) {
-                // console.error(key + ' 1 ' + type);
-                // console.error('  ' + type + '-' + operator + ' ' + vari);
                 let bool: boolean;
                 switch (operator) {
                   case '==':
@@ -197,28 +186,60 @@ export class AppComponent {
             });
           }
         });
-        // if we have array of values then traspile it to or independent expressions
+        // if we have array of values then traspile it to independent expressions
+        // for example 'f | t | f'
         operator = orOperator.join(' | ');
 
-        // add parenthesis as the ruleString said
+        // add parenthesis as the ruleString
         if (parenthesis === '(') {
           ruleString += ' ( ';
         } else if (parenthesis === ')') {
           ruleString += ' ) ';
         }
+        // replace the expression with the equal/result boolean T|F
         ruleString = ruleString.replace(rule, operator);
       });
-      res = ruleString;
-      ruleSet.boolExpression = res;
+      ruleBooleanFormat = ruleString;
+      // set the boolean format fo rule to ruleSet
+      ruleSet.boolExpression = ruleBooleanFormat;
 
-      let ast = parseScript(res);
+      // convert the boolean expression to AST format (based on ChatGPT)
+      let ast = parseScript(ruleBooleanFormat);
+      // take the result of ruleBooleanFormat
       let result = this.evaluate(ast.body[0]);
+
       //  the result of rule stored on ruleSet, ready to present it on html
       ruleSet.result = result;
       // add to array of rules
-      this.ruless.push(ruleSet);
+      this.rules.push(ruleSet);
     });
   }
+
+  protected evaluate = (node: any): boolean => {
+    if (node.type === 'LogicalExpression' || node.type === 'BinaryExpression') {
+      const left = this.evaluate(node.left);
+      const right = this.evaluate(node.right);
+      switch (node.operator) {
+        case '&':
+          return left && right;
+        case '|':
+          return left || right;
+        default:
+          throw new Error(`Unknown operator: ${node.operator}`);
+      }
+    } else if (node.type === 'UnaryExpression') {
+      return !node.argument;
+    } else if (node.type === 'ExpressionStatement') {
+      return this.evaluate(node.expression);
+    } else if (node.type === 'Literal') {
+      return node.value;
+    } else if (node.type === 'Identifier') {
+      // Assume that the identifier refers to a boolean value in the environment.
+      return node.value;
+    } else {
+      throw new Error(`Unknown node type: ${node.type}`);
+    }
+  };
 
   ipReputationRiskLevels: IPReputationRiskLevelEnum[] = [
     IPReputationRiskLevelEnum.Low,
